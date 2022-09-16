185849c8d8b54b590f77bc5c8c980fc6eda6af6f
Vlad R <Rozhukvlad@gmail.com>
2022-09-16 20:12:18 +0300

    solution task3

---
+++ lesson2/task3/index.js
@@ -0,0 +1,18 @@
+let goodNumber = 17;
+let anotherNumber = goodNumber;
+goodNumber = 77;
+
+console.log(goodNumber);
+console.log(anotherNumber);
+
+const customer = {
+    name: 'John',
+    age: 17,
+};
+
+let anotherCustomer = customer;
+
+anotherCustomer.age = 20;
+
+console.log(customer.age);
+console.log(anotherCustomer.age);
