0eb62f277061ce90eaf0f4896afde57c3d15f521
Vlad R <Rozhukvlad@gmail.com>
2022-09-20 21:09:05 +0300

    solution hometask4

---
+++ Lesson6/hometask4/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @param {number} delta
+ * @return {number[]}
+ */
+const increaseEvenEl = (arr, delta) => {
+  if (!Array.isArray(arr)) return null;
+  const resArr = [];
+  for (const num of arr) {
+    if (num % 2 === 0) resArr.push(num + delta);
+    if (num % 2 === 1) resArr.push(num);
+  }
+  return resArr;
+};
+
+// examples
+console.log(increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20)); // ===> [22, 5, 26, 28, 11, 9, 24]
+console.log(increaseEvenEl([7, 11, 1], 10)); // ===> [7, 11, 1]
+console.log(increaseEvenEl([], 120)); // ===> []
