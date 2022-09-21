881a4b9501d7f3424f6f8aace22758be8562b2ca
Vlad R <Rozhukvlad@gmail.com>
2022-09-21 22:41:52 +0300

    Solution addtask

--- lesson5/addtask/index.js
+++ lesson5/addtask/index.js
@@ -3,6 +3,20 @@
  * @return {undefined}
  */
 function getPrimes(num) {
-    for (let i = 2; i <= num; i += 1) {}
+  for (let i = 2; i <= num; i += 1) {
+    let count = 0;
+    for (let k = 2; k <= i; k += 1) {
+      if (i % k === 0) {
+        count += 1;
+      }
+      if (count > 1) {
+        break;
+      }
+    }
+    if (count === 1) {
+      console.log(i);
+    }
+  }
 }
-getPrimes(45);
+
+getPrimes(122);
