6f42e5b75017ab6f23750a305fdfa33fc54ebf63
Vlad R <Rozhukvlad@gmail.com>
2022-09-21 18:55:39 +0300

    solution hometask12

---
+++ Lesson6/hometask12/index.js
@@ -0,0 +1,16 @@
+function removeDuplicates(array) {
+  if (!array.length) return null;
+  const getNewArr = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (i === 0) {
+      getNewArr.push(array[0]);
+    }
+    if (!getNewArr.includes(array[i])) {
+      getNewArr.push(array[i]);
+    }
+  }
+  return getNewArr;
+}
+
+console.log(removeDuplicates([434, 434, 234, 34, 43, 34, 34, 232, 54, 1, 3, 7]));
