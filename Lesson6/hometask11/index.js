bc1c9c1427a915994a72e938d4b8e95d8c9ae233
Vlad R <Rozhukvlad@gmail.com>
2022-09-21 18:33:37 +0300

    solution hometask11

---
+++ Lesson6/hometask11/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {number} num
+ * @return {boolean}
+ */
+const includes = (arr, num) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === num) return true;
+  }
+  return false;
+};
+
+// examples
+console.log(includes([1, 4, 8, 3], 3)); // ==> true
+console.log(includes([1, 4, 8, 3], 5)); // ==> false
