afc73a31a47e0998c47d7026ac0f05183105e948
Mwxyz2022 <rozhukvlad@gmail.com>
2022-10-27 22:47:33 +0300

    refactoring task

--- lesson7/hometask6/index.js
+++ lesson7/hometask6/index.js
@@ -1,7 +1,10 @@
-const sum = arr => {
-  if (!Array.isArray(arr)) return null;
-  const sumArr = arr.reduce((acc, elem) => acc + elem, 0);
-  return sumArr;
-};
+const sum = arr => (Array.isArray(arr) ? arr.reduce((acc, num) => acc + num, 0) : null);
+//
+// const sum = arr => {
+//   if (!Array.isArray(arr)) return null;
+//   const sumArr = arr.reduce((acc, elem) => acc + elem, 0);
+//   return sumArr;
+// };
 
-console.log(sum([23, 4, 2, 3, 134, 34]));
+console.log(sum([23, 4, 2, 3, 134, 34, 1]));
+console.log(sum(2));
