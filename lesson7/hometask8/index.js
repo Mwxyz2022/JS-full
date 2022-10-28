08729c846dbf257d6f01ef04b199a273e527bdbd
Mwxyz2022 <rozhukvlad@gmail.com>
2022-10-28 13:49:29 +0300

    solution & refactoring two last task in 7 lesson & last task in 5 lesson

--- lesson7/hometask8/index.js
+++ lesson7/hometask8/index.js
@@ -1,3 +1,38 @@
-const arrAverage = arr => arr.reduce((acc, num) => acc + num) / arr.length;
+// //requirement
+//
+// input: [array]
+// output: integer number
 
-console.log(arrAverage([12, 15, 16, 17, 20]));
+// algorithm
+//
+// _1. если инпут не массив, вернуть null;
+//
+// _2. среднее арифм. всех эл. массива.
+//
+// _3.  вернуть результат number
+
+//    Draft solution    //
+
+// function arrAverage(array) {
+//   if (!Array.isArray(array)) {
+//     return null;
+//   }
+//   let average = null;
+
+//   for (let index = 0; index < array.length; index += 1) {
+//     average += array[index];
+//   }
+//   return average / array.length;
+// }
+
+//    Refactoring    //
+
+const arrAverage = inpArray =>
+  Array.isArray(inpArray) ? inpArray.reduce((acc, num) => acc + num) / inpArray.length : null;
+
+// testData
+console.log(arrAverage([3, 2, 3]));
+console.log(arrAverage([3, NaN, 3]));
+console.log(arrAverage([3, 2, undefined]));
+console.log(arrAverage([3, null, 3]));
+console.log(arrAverage(2));
