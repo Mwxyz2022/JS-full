7d0ae21834c2f4add89c1b1d9e76a332f93a2e6f
Mwxyz2022 <rozhukvlad@gmail.com>
2022-11-04 18:45:59 +0200

    solution hometask1

--- lesson17/hometask1/index.js
+++ lesson17/hometask1/index.js
@@ -8,10 +8,10 @@ export function sayName() {
 }
 
 // вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента
-// ... your code here
+sayName.call(student);
 
 // вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
-// ... your code here
+sayName.call({ name: 'Bruce' });
 
 /* ===> 2 <=== */
 const company = {
@@ -25,7 +25,7 @@ function greeting(firstName, lastName) {
 // вызовите ф-цию greeting так, чтобы в консоль вывелось
 // 'Hello, Bob Marley. Welcome to the Microsoft'
 // используйте объект company
-// ... your code here
+greeting.call(company, 'Bob', 'Marley');
 
 /* ===> 3 <=== */
 const country = {
@@ -42,7 +42,8 @@ function getPopulation(population) {
 // 43000 передавайте в виде числа
 // используйте объект country
 // результат работы ф-ции getPopulation присвойте в переменную и выведите в консоль
-// ... your code here
+const populationUa = getPopulation.call(country, 43000);
+console.log(populationUa);
 
 /* ===> 4 <=== */
 const transaction = {
@@ -65,4 +66,4 @@ const anotherTransaction = {
 // вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
 // '400 USD - buy on NASDAQ'
 // используйте объект anotherTransaction как контекст
-// ... your code here
+transaction.printTransaction.call(anotherTransaction);
