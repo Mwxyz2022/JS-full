a0e49402d4646b628f4ba73ab63698e9f6a2f2cf
Mwxyz2022 <rozhukvlad@gmail.com>
2022-11-14 13:29:55 +0200

    solution hometask9

--- lesson21/hometask9/index.js
+++ lesson21/hometask9/index.js
@@ -1,6 +1,15 @@
 //        1.  Экспортируемая Фу-я finishFrom()
 //        2.  Должна вставить:
-//            * input text-type name='login'
-//            * input type-password
+//            * input type - text name='login'
+//            * изменить type c text на -password
 
-const finishFrom = () => {};
+export const finishForm = () => {
+  const logForm = document.querySelector('.login-form');
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.name = 'login';
+  logForm.prepend(input);
+
+  const changeInp = document.querySelector('[name = password]');
+  changeInp.type = 'password';
+};
