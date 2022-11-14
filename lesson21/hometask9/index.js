234e14b547daa671ff17b55a7b9b692f7e033fb1
Mwxyz2022 <rozhukvlad@gmail.com>
2022-11-14 12:43:39 +0200

    solution hometask lesson21

---
+++ lesson21/hometask9/index.js
@@ -0,0 +1,6 @@
+//        1.  Экспортируемая Фу-я finishFrom()
+//        2.  Должна вставить:
+//            * input text-type name='login'
+//            * input type-password
+
+const finishFrom = () => {};
