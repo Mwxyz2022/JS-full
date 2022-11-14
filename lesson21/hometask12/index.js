a57c016fd0008d98cf2567b2bfb04a03f58dafe7
Mwxyz2022 <rozhukvlad@gmail.com>
2022-11-14 15:42:57 +0200

    solution task

---
+++ lesson21/hometask12/index.js
@@ -0,0 +1,11 @@
+//      1.Экспортируемая ф-я getSection(num)
+//        * num - значение атрибута data-number эл.
+//        * вернет значение атрибута data-section родителя.
+
+// export
+const getSection = num =>
+  document.querySelector(`span[data-number="${num}"]`).closest('.box').getAttribute('data-section');
+
+// console.log(getSection('3'));
+// console.log(getSection('2'));
+// console.log(getSection('5'));
