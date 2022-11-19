ec289c652c9cc23d896a79a8de32e82d617a5fcf
Mwxyz2022 <rozhukvlad@gmail.com>
2022-11-19 13:37:14 +0200

    solution

---
+++ lesson24/task2/index.js
@@ -0,0 +1,13 @@
+// 1. форматер с настройками отображения часов и минут.
+// 2. (Экспортируемая) фу-я  inp: Date out: UTC'hour:minute'
+const formatter = new Intl.DateTimeFormat('en', {
+  timeZone: 'UTC',
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
+});
+
+// export
+export const getGreenwichTime = date => formatter.format(date);
+
+console.log(getGreenwichTime(new Date()));
