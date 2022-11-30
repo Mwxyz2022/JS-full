0cbbdfa885a9210d6afc862d45b7013eb2bc892c
Mwxyz2022 <rozhukvlad@gmail.com>
2022-11-30 22:16:49 +0200

    solution

---
+++ test.js
@@ -0,0 +1,19 @@
+// const myObj = {
+//   a: 1,
+//   b: 2,
+//   c: { a: 13, b: 12 },
+//   d: [13, 25, 15],
+// };
+
+// const {
+//   b: firstb,
+//   d: [, arrTwo, ...lyaps],
+//   c: { a: lastA },
+//   ...tail
+// } = myObj || {};
+
+// console.log(firstb);
+// console.log(arrTwo);
+// console.log(lyaps);
+// console.log(lastA);
+// console.log(tail);
