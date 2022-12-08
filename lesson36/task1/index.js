2e696367b395f623f62aeb554bbc53d616604354
Mwxyz2022 <rozhukvlad@gmail.com>
2022-12-09 01:00:33 +0200

    solution

---
+++ lesson36/task1/index.js
@@ -0,0 +1,16 @@
+export const fetchUser = async userId => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    if (!response.ok) {
+      return null;
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (err) {
+    throw new Error('Failed to get user data');
+  }
+};
+
+fetchUser('google')
+  .then(data => console.log(data))
+  .catch(err => alert(err.message));
